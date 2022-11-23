f1270f665c666695e96b72a7ff9753a33c93323c
Taylor Alan <tay10alan60@gmail.com>
2022-11-23 10:15:27 -0800

    submit half finished

--- client/Components/UserProfile.jsx
+++ client/Components/UserProfile.jsx
@@ -30,11 +30,126 @@ function UserProfile(props) {
         //setPets()
     }, [])
 
-    
-
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if(details.name && details.species && details.breed){
+        fetch('api/pets', {
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                name: details.name,
+                species: details.species.replace(' ', '-'),
+                breed: details.breed,
+                weight: details.weight,
+                color: details.color,
+                age: details.age,
+                owner_id: props.userId,
+                vet_id: 2
+              })})
+        .then((data) => {
+            if(data.status === 200)
+             return data.json()
+            else{
+                throw new Error("data status was not 200")
+            }})
+      }
+    else{
+        alert('You must enter Name, Species, and Breed')
+    }};
+      const [error, setError] = useState('');
+      //must be filled, name, species, breed
+      //vet id to be 2 integer
+      //send back id and username
+      const [petDetails, setPetDetails] = useState({ name: '', species: '', breed: '', weight: null, color : '', age: null });
     return (
         <div id="container">
             <p id="greeting">{props.username}'s Profile</p>
+            
+        <form className='pet-form' onSubmit={submitHandler}>
+      <div className='form-inner'>
+        <h2>Add New Pet!</h2>
+        <div className="formContainer">
+        {error != ' ' ? <div className='error'></div> : ''}
+        <div className='form-group'>
+          <label htmlFor='name'>Name:</label>
+          <input
+            autoComplete='off'
+            type='text'
+            name='name'
+            id='name'
+            onChange={(e) => setPetDetails({ ...petDetails, name: e.target.value })}
+            value={petDetails.name}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Species:</label>
+          <input
+            autoComplete='off'
+            type='password'
+            name='password'
+            id='password'
+            onChange={(e) =>
+              setPetDetails({ ...petDetails, species: e.target.value })
+            }
+            value={petDetails.password}
+          />
+          </div>
+          <div className='form-group'>
+          <label htmlFor='name'>Breed:</label>
+          <input
+            autoComplete='off'
+            type='text'
+            name='name'
+            id='name'
+            onChange={(e) => setPetDetails({ ...petDetails, breed: e.target.value })}
+            value={petDetails.name}
+          />
+        </div>
+        </div>
+        <div className='formContainer'>
+        <div className='form-group'>
+          <label htmlFor='name'>Weight:</label>
+          <input
+            autoComplete='off'
+            type='text'
+            name='name'
+            id='name'
+            onChange={(e) => setPetDetails({ ...petDetails, weight: e.target.value })}
+            value={petDetails.name}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='name'>Color:</label>
+          <input
+            autoComplete='off'
+            type='text'
+            name='name'
+            id='name'
+            onChange={(e) => setPetDetails({ ...petDetails, color: e.target.value })}
+            value={petDetails.name}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='name'>Age:</label>
+          <input
+            autoComplete='off'
+            type='text'
+            name='name'
+            id='name'
+            onChange={(e) => setPetDetails({ ...petDetails, age: e.target.value })}
+            value={petDetails.name}
+          />
+        </div>
+        </div>
+        <div>
+          <input id="submitPet" type='submit' value='SUBMIT' />
+        </div>
+        </div>
+    </form>
+        
             <div id="cardContainer">
             {
                 petInfo.map(pet => {
